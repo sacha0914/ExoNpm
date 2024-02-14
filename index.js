b4fbd597825dc366c1b9e632dbfc5d2e3df9bfb3
GianniB77 <giannibosio.gb@outlook.fr>
2024-02-14 12:53:49 +0100

    trolling

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function sayHello() {
   const date = new Date().toLocaleDateString();  
-    return "Hello, World! Today is the " + date + ".";
+    return "Hello, World! Today is the " + date + ". BTW je ne pense pas être un moteur pour cette classe.";
 }
 
 module.exports = sayHello;
